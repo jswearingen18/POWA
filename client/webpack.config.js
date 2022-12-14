7bb442213061ca753deb1ff99c7e391aa3def9b7
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-12-13 18:35:43 -0600

    updated file structure

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const { InjectManifest, GenerateSW } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -23,6 +23,8 @@ module.exports = () => {
         title: "Jate PWA",
       }),
 
+      new GenerateSW(),
+
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
@@ -36,8 +38,8 @@ module.exports = () => {
         description: "PWA called Jate",
         background_color: "#465E69",
         theme_color: "#8dd6f9",
-        start_url: "./",
-        publicPath: "./",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
